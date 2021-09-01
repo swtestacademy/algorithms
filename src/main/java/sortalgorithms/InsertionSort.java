2954ac79b727a720f1457bfbc032bb21dd5bbadb
Onur Baskirt <onur.baskirt@dnata.com>
2021-09-02 01:51:48 +0400

    Naming changes.

--- src/main/java/sortalgorithms/InsertionSort.java
+++ src/main/java/sortalgorithms/InsertionSort.java
@@ -11,52 +11,52 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] unsortedArray = { 18, 32, -11, 6, 68, 2, -34 };
         System.out.println("Ascending Sorted Array: " + Arrays.toString(ascendingInsertionSort(unsortedArray)));
-        System.out.println("Ascending Sorted Array Simpler: " + Arrays.toString(ascendingInsertionSortSimpler(unsortedArray)));
+        System.out.println("Ascending Sorted Array Simpler: " + Arrays.toString(ascendingInsertionSortSimple(unsortedArray)));
         System.out.println("Descending Sorted Array: " + Arrays.toString(descendingInsertionSort(unsortedArray)));
     }
 
-    public static int[] ascendingInsertionSort(int[] unsortedArray) {
+    public static int[] ascendingInsertionSortSimple(int[] unsortedArray) {
         int[] array = Arrays.copyOf(unsortedArray, unsortedArray.length);
-        for (int indexToBeSorted = 1; indexToBeSorted < array.length; indexToBeSorted++) {
-            int elementToBeSorted = array[indexToBeSorted];
+        for (int indexStartingToSort = 1; indexStartingToSort < array.length; indexStartingToSort++) {
+            int elementStartingToSort = array[indexStartingToSort];
             int i;
-            for (i = indexToBeSorted; i > 0; i--) {
-                if (elementToBeSorted < array[i - 1]) {
-                    array[i] = array[i - 1];
-                    if (i == 1) {
-                        array[0] = elementToBeSorted;
-                    }
-                } else {
-                    array[i] = elementToBeSorted;
-                    break;
-                }
-            }
-        }
-        return array;
-    }
-
-    public static int[] ascendingInsertionSortSimpler(int[] unsortedArray) {
-        int[] array = Arrays.copyOf(unsortedArray, unsortedArray.length);
-        for (int indexToBeSorted = 1; indexToBeSorted < array.length; indexToBeSorted++) {
-            int elementToBeSorted = array[indexToBeSorted];
-            int i;
-            for (i = indexToBeSorted; i > 0 && elementToBeSorted < array[i - 1]; i--) {
+            for (i = indexStartingToSort; i > 0 && elementStartingToSort < array[i - 1]; i--) {
                 array[i] = array[i - 1];
             }
-            array[i] = elementToBeSorted;
+            array[i] = elementStartingToSort;
         }
         return array;
     }
 
     public static int[] descendingInsertionSort(int[] unsortedArray) {
         int[] array = Arrays.copyOf(unsortedArray, unsortedArray.length);
-        for (int indexToBeSorted = 1; indexToBeSorted < array.length; indexToBeSorted++) {
-            int elementToBeSorted = array[indexToBeSorted];
+        for (int indexStartingToSort = 1; indexStartingToSort < array.length; indexStartingToSort++) {
+            int elementStartingToSort = array[indexStartingToSort];
             int i;
-            for (i = indexToBeSorted; i > 0 && elementToBeSorted > array[i - 1]; i--) {
+            for (i = indexStartingToSort; i > 0 && elementStartingToSort > array[i - 1]; i--) {
                 array[i] = array[i - 1];
             }
-            array[i] = elementToBeSorted;
+            array[i] = elementStartingToSort;
+        }
+        return array;
+    }
+
+    public static int[] ascendingInsertionSort(int[] unsortedArray) {
+        int[] array = Arrays.copyOf(unsortedArray, unsortedArray.length);
+        for (int indexStartingToSort = 1; indexStartingToSort < array.length; indexStartingToSort++) {
+            int elementStartingToSort = array[indexStartingToSort];
+            int i;
+            for (i = indexStartingToSort; i > 0; i--) {
+                if (elementStartingToSort < array[i - 1]) {
+                    array[i] = array[i - 1];
+                    if (i == 1) {
+                        array[0] = elementStartingToSort;
+                    }
+                } else {
+                    array[i] = elementStartingToSort;
+                    break;
+                }
+            }
         }
         return array;
     }
